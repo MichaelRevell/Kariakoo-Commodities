748d73ea30ce974576c3fd7014dac94e6590e8da
Michael <mars8082686@gmail.com>
2012-01-20 20:23:42 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kanakoo::Application.config.secret_token = '45dc329310d29ddf7444883623e7ccd1cc24f85342e9e2cf60a11288fa0517e0b18c78c069b081226d7bd37e6405c9521561649e7bb1f00da110e2e8f7a260e7'
