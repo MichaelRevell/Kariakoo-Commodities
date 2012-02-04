bb64bb5af4c32aefe9e4de50e0ed472ac0b95ac0
Daniel Gaviria <ggdaniel@gmail.com>
2012-02-03 20:01:07 -0500

    corrected app title

--- app/models/commodity.rb
+++ app/models/commodity.rb
@@ -1,6 +1,6 @@
 class Commodity < ActiveRecord::Base
 #	has_one retail_unit, :class_name => 'unit'
 #	has_one wholesale_unit, :class_name => 'unit'
-#	has_one farm_gate_unit, :class_name => 'unit'	
+#	has_one farmgate_unit, :class_name => 'unit'	
 #	has_one delivered_unit, :class_name => 'unit'		
 end
